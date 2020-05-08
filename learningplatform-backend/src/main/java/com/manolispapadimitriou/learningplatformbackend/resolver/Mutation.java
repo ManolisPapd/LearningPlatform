11220a53f528bb83756f4f6c30d2af47bc1e1e42
mpapd <valkon.gr@gmail.com>
2020-05-08 23:10:21 +0300

    Overall fixes

---
+++ learningplatform-backend/src/main/java/com/manolispapadimitriou/learningplatformbackend/resolver/Mutation.java
@@ -0,0 +1,4 @@
+package com.manolispapadimitriou.learningplatformbackend.resolver;
+
+public class Mutation {
+}
--- learningplatform-dashboard/src/components/Quiz/QuizComponent/QuizComponent.js
+++ learningplatform-dashboard/src/components/Quiz/QuizComponent/QuizComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Form,Button} from 'react-bootstrap';
+import ReactAce from 'react-ace-editor';
 import './QuizComponent.css';
 
 class QuizComponent extends Component {
@@ -23,12 +24,16 @@ class QuizComponent extends Component {
     }
 
     handleQuizSumbition = () => {
-
+        console.log("Quiz Submition")
     }
 
+    handleCodeSubmition = (e) => {
+        console.log(e)
+    }
     
 
     render (){
+
 
         return(
             <React.Fragment>
@@ -48,35 +53,30 @@ class QuizComponent extends Component {
                                     </Form.Control>
                                     
                                 </Form.Group>
+                                <Button className="quizSubmit" variant="primary" onClick={this.handleQuizSumbition}>Submit Answer!</Button>
                             </Form>
                         }
-                        {/* Present multiple choice quiz */}
+                        {/* Present code quiz */}
                         { (this.state.currentQuiz === i && quiz.details && quiz.type === 'code') &&
-                            <h1>HERE CODE</h1>
+                            <React.Fragment>
+                                <img src={quiz.details} className="courseImage" alt="Cinque Terre"/> 
+                            
+                                <ReactAce
+                                    mode="mysql"
+                                    theme="eclipse"
+                                    setReadOnly={false}
+                                    onChange={this.handleCodeSubmition}
+                                    style={{ height: '200px' }}
+                                    ref={instance => { this.ace = instance; }} 
+                                />
+                            </React.Fragment>
+                            
+                            
                         }
                         
                     </div>
                 ))}
 
-            {/* //     {this.state.currentQuiz === 0 &&
-            //         <Form>
-            //             <Form.Group controlId="exampleForm.ControlSelect2">
-            //                 <Form.Label className="parentLabel">Multiple choice question, select only one!<br/><div className="customLabel"></div></Form.Label> 
-            //                 <div className="customLabel">What is this boogy doogy sceletal * from user? What is this boogy doogy sceletal * from user?</div><br/>
-            //                 <Form.Control className="selectOptionsForm"  as="select" multiple onChange={this.handleSelection}>
-            //                     <option value="letter-answer-a" className="optionClass">a - Don't know</option>
-            //                     <option value="letter-answer-b" className="optionClass">b - It's okey</option>
-            //                     <option value="letter-answer-c" className="optionClass">c - Sexy BoooooooooooooyBoooooo</option>
-            //                     <option value="letter-answer-d" className="optionClass">d - I am not your boy toy</option>
-            //                 </Form.Control>
-                            
-            //             </Form.Group>
-            //         </Form>
-            //     }
-
-            // {this.state.currentQuiz === 1 &&
-            //         <div>Y SNAO</div>
-            // } */}
                 <h3>Question: {this.state.currentQuiz + 1} / {this.props.quizzes.length}</h3>
                 {this.state.currentQuiz > 0 ? ( <Button variant="success" onClick={this.handleQuizNavigationPrevious}> <i className="fa fa-angle-left"></i> previous</Button>)
                     :<Button variant="secondary" disabled><i className="fa fa-angle-left"></i> previous</Button>
@@ -86,7 +86,7 @@ class QuizComponent extends Component {
                 }
                 <br /> <br />
 
-                {this.state.currentQuiz === this.props.quizzes.length - 1 && <Button variant="primary" onClick={this.handleQuizSumbition}>End Quiz!</Button>}
+                
             </React.Fragment>
         )
     }
