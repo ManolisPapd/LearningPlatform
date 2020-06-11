743cc119cf90372b6c006c9f38d83cae38ce9c70
mpapd <valkon.gr@gmail.com>
2020-06-11 22:17:23 +0300

    Added new learning material

--- learningplatform-backend/src/main/resources/data.sql
+++ learningplatform-backend/src/main/resources/data.sql
@@ -110,7 +110,19 @@ values (4,'Section 2: Syntax and Tables',1,'{
                 },
                 {
     				"title": "Advanced Table Statements",
-    				"paragraph" : " A SQL DROP TABLE statement is used to delete a table definition and all data from a table, The DELETE statement is used to delete rows from a table. If you want to remove a specific row from a table you should use WHERE condition, SQL RENAME TABLE syntax is used to change the name of a table. Sometimes, we choose non-meaningful name for the table. So it is required to be changed. A truncate SQL statement is used to remove all rows (complete data) from a table. It is similar to the DELETE statement with no WHERE clause. Truncate table is faster and uses lesser resources than DELETE TABLE command. The ALTER TABLE statement is used to add, modify or delete columns in an existing table. It is also used to rename a table. You can also use SQL ALTER TABLE command to add and drop various constraints on an existing table."
+    				"paragraph" : "A SQL DROP TABLE statement is used to delete a table definition and all data from a table, The DELETE statement is used to delete rows from a table. If you want to remove a specific row from a table you should use WHERE condition, SQL RENAME TABLE syntax is used to change the name of a table. Sometimes, we choose non-meaningful name for the table. So it is required to be changed. A truncate SQL statement is used to remove all rows (complete data) from a table. It is similar to the DELETE statement with no WHERE clause. Truncate table is faster and uses lesser resources than DELETE TABLE command. The ALTER TABLE statement is used to add, modify or delete columns in an existing table. It is also used to rename a table. You can also use SQL ALTER TABLE command to add and drop various constraints on an existing table."
+
+                },
+                {
+    				"title": "Primary Key",
+                    "image" : "https://i.imgur.com/6soiOAV.png",
+    				"paragraph" : "A column or columns is called primary key (PK) that uniquely identifies each row in the table. If you want to create a primary key, you should define a PRIMARY KEY constraint when you create or modify a table. When multiple columns are used as a primary key, it is known as composite primary key. In designing the composite primary key, you should use as few columns as possible. It is good for storage and performance both, the more columns you use for primary key the more storage space you require. Inn terms of performance, less data means the database can process faster. Primary key enforces the entity integrity of the table, always has unique data, a primary key length cannot be exceeded than 900 bytes, a primary key cannot have null value, there can be no duplicate value for a primary key and a table can contain only one primary key constraint. The main advantage of this uniqueness is that we get fast access."
+
+                },
+                {
+    				"title": "Foreign Key",
+    				"image" : "https://i.imgur.com/2vjDpdK.png",
+                    "paragraph" : "In the relational databases, a foreign key is a field or a column that is used to establish a link between two tables. In simple words you can say that, a foreign key in one table used to point primary key in another table. The foreign key constraint is generally prevents action that destroy links between tables.It also prevents invalid data to enter in foreign key column."
 
                 },
                 {
@@ -175,13 +187,271 @@ values (4,'Section 2: Syntax and Tables',1,'{
 }');
 
 insert into SECTION(id,name,course_id,information)
-values (5,'Section 3: Advanced queries',1,'{"id" : "text_material", "paragraph": "TEXT MATERIAL"}');
+values (5,'Section 3: Advanced Queries',1,'{
+    "id": "text_material",
+    "paragraph": [
+    			{
+    				"title": "Sum",
+                    "image" : "https://i.imgur.com/8u3bM4Y.png",
+    				"paragraph" : "It is also known as SQL SUM() function. It is used in a SQL query to return summed value of an expression. If you want to know how the combined total salary of all employee whose salary is above 20000 per month."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/AfzcA76.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Date",
+                    "image" : "",
+    				"paragraph" : "SQL SELECT DATE is used to retrieve a date from a database. If you want to find a particular date from a database, you can use this statement."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/EeMl8ae.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/MAR0Cfu.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Handling Empty Data ",
+                    "image" : "",
+    				"paragraph" : "Null values are used to represent missing unknown data. There can be two conditions: Where SQL is NULL and Where SQL is NOT NULL. If in a table, a column is optional, it is very easy to insert data in column or update an existing record without adding a value in this column. This means that field has null value. Note: we should not compare null value with 0. They are not equivalent."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/Nh3PrW0.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/Rnc67uN.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Distinct",
+                    "image" : "",
+    				"paragraph" : "The SQL DISTINCT command is used with SELECT key word to retrieve only distinct or unique data.In a table, there may be a chance to exist a duplicate value and sometimes we want to retrieve only unique values. In such scenarios, SQL SELECT DISTINCT statement is used. Note: SQL SELECT UNIQUE and SQL SELECT DISTINCT statements are same."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/rNvhMjF.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Order By ",
+                    "image" : "https://i.imgur.com/ZIGEnlu.png",
+    				"paragraph" : "The SQL ORDER BY clause is used for sorting data in ascending and descending order based on one or more columns. Some databases sort query results in ascending order by default. Ascending Order: This statement is used to sort data in ascending order. If you miss the ASC attribute, SQL ORDER BY query takes ascending order by default. Descebdubg Order: This statement is used to sort data in descending order. You should use the DESC attribute in your ORDER BY clause as follows."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/0AbN3kK.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Update",
+                    "image" : "",
+    				"paragraph" : "The SQL commands (UPDATE and DELETE) are used to modify the data that is already in the database. The SQL DELETE command uses a WHERE clause. SQL UPDATE statement is used to change the data of the records held by tables. Which rows is to be update, it is decided by a condition. To specify condition, we use WHERE clause."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/8eIhGZP.png",
+    				"paragraph" : ""
+
+                }
+    	]
+}');
 
 insert into SECTION(id,name,course_id,information)
-values (6,'Section 4: Join Clauses',1,'{"id" : "text_material", "paragraph": "TEXT MATERIAL"}');
+values (6,'Section 4: Join Clauses',1,'{
+    "id": "text_material",
+    "paragraph": [
+    			{
+    				"title": "The Concept of Joining Tables ",
+                    "image" : "https://lucysong0622.files.wordpress.com/2018/09/inner-join.gif?w=329&h=227",
+                    "video" : "KTvYHEntvn8",
+                    "paragraph" : "As the name shows, JOIN means to combine something. In case of SQL, JOIN means \"to combine two or more tables\". The SQL JOIN clause takes records from two or more tables in a database and combines it together. ANSI standard SQL defines five types of JOIN : inner join, left outer join, right outer join, full outer join and cross join. In the process of joining, rows of both tables are combined in a single table."
+                },
+                {
+    				"title": "",
+                    "image" : "https://www.w3schools.com/sql/img_innerjoin.gif",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "Why SQL JOIN is used?",
+                    "image" : "",
+                    "paragraph" : "If you want to access more than one table through a select statement. If you want to combine two or more table then SQL JOIN statement is used .it combines rows of that tables in one table and one can retrieve the information by a SELECT statement. The joining of two or more tables is based on common field between them. SQL INNER JOIN also known as simple join is the most common type of join."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/Hop9Dkn.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "RIGHT JOIN ",
+                    "image" : "https://www.w3schools.com/sql/img_rightjoin.gif",
+    				"paragraph" : "The SQL right join returns all the values from the rows of right table. It also includes the matched values from left table but if there is no matching in both tables, it returns NULL."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/EGg1Lv4.png",
+                    "paragraph" : ""
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/W2Cjw1a.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "LEFT JOIN ",
+                    "image" : "https://www.w3schools.com/sql/img_leftjoin.gif",
+    				"paragraph" : "The SQL left join returns all the values from the left table and it also includes matching values from right table, if there are no matching join value it returns NULL."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/V6wKXEr.png",
+                    "paragraph" : ""
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/gI8GSJX.png",
+    				"paragraph" : ""
+
+                },
+                {
+    				"title": "FULL JOIN",
+                    "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSttmxIcQUhzEAqXMXMj2ve4-VRj8A4Nuee4jl6fHStPFJMIuSF&usqp=CAU",
+                    "paragraph" : "The SQL full join is the result of combination of both left and right outer join and the join tables have all the records from both tables. It puts NULL on the place of matches not found. SQL full outer join is used to combine the result of both left and right outer join and returns all rows (don''t care its matched or unmatched) from the both participating tables."
+
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/8LOpnOK.png",
+    				"paragraph" : ""
+
+                }
+    	]
+}');
 
 insert into SECTION(id,name,course_id,information)
-values (7,'Section 5: Triggers',1,'{"id" : "text_material", "paragraph": "TEXT MATERIAL"}');
+values (7,'Section 5: Triggers',1,'{
+    "id": "text_material",
+    "paragraph": [
+    			{
+    				"title": "Trigger",
+                    "image" : "",
+                    "paragraph" : "A trigger is a stored procedure in database which automatically invokes whenever a special event in the database occurs. For example, a trigger can be invoked when a row is inserted into a specified table or when certain table columns are being updated. SQL Server triggers are special stored procedures that are executed automatically in response to the database object, database, and server events. SQL Server provides three type of triggers: Data manipulation language (DML) triggers which are invoked automatically in response to INSERT, UPDATE, and DELETE events against tables, Data definition language (DDL) triggers which fire in response to CREATE, ALTER, and DROP statements. DDL triggers also fire in response to some system stored procedures that perform DDL-like operations and Logon triggers which fire in response to LOGON events"
+                },
+                {
+    				"title": "",
+                    "image" : "",
+                    "video" : "k0S4P-a6d5w",
+                    "paragraph" : ""
+                },
+                {
+    				"title": "Create Trigger",
+                    "image" : "",
+                    "paragraph" : "The CREATE TRIGGER statement allows you to create a new trigger that is fired automatically whenever an event such as INSERT, DELETE, or UPDATE occurs against a table."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/USS2REA.png",
+                    "paragraph" : "The schema_name is the name of the schema to which the new trigger belongs. The schema name is optional. The trigger_name is the user-defined name for the new trigger. The table_name is the table to which the trigger applies. The event is listed in the AFTER clause. The event could be INSERT, UPDATE, or DELETE. A single trigger can fire in response to one or more actions against the table. The NOT FOR REPLICATION option instructs SQL Server not to fire the trigger when data modification is made as part of a replication process. The sql_statements is one or more Transact-SQL used to carry out actions once an event occurs."
+                },
+                {
+    				"title": "Practical Example of a Trigger Creation",
+                    "image" : "",
+                    "paragraph" : ""
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/YaEST2E.png",
+                    "paragraph" : ""
+                },{
+    				"title": "",
+                    "image" : "https://i.imgur.com/bsOWv8Z.png",
+                    "paragraph" : "Above SQL statement will create a trigger in the student database in which whenever subjects marks are entered, before inserting this data into the database, trigger will compute those two values and insert with the entered values. i.e.."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/iOU7CA4.png",
+                    "paragraph" : "In this way trigger can be creates and executed in the databases."
+                },
+                {
+    				"title": "Disabling a Trigger",
+                    "image" : "",
+                    "paragraph" : "Sometimes, for the troubleshooting or data recovering purpose, you may want to disable a trigger temporarily."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/l5FiJc9.png",
+                    "paragraph" : "In this syntax: First, specify the name of the schema to which the trigger belongs and the name of the trigger that you want to disable after the DISABLE TRIGGER keywords. Second, specify the table name or view that the trigger was bound to if the trigger is a DML trigger. Use DATABASE if the trigger is DDL database-scoped trigger, or SERVER if the trigger is DDL server-scoped trigger. An example below."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/ukf3VcN.png",
+                    "paragraph" : ""
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/iOU7CA4.png",
+                    "paragraph" : "In this way trigger can be creates and executed in the databases."
+                },
+                {
+    				"title": "Enabling a Trigger",
+                    "image" : "",
+                    "paragraph" : "The ENABLE TRIGGER statement allows you to enable a trigger so that the trigger can be fired whenever an event occurs."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/l1o2kro.png",
+                    "paragraph" : "In this syntax: First, specify the name of the trigger that you want to enable. Optionally, you can specify the name of the schema to which the trigger belongs.Second, specify the table to which the trigger belongs if the trigger is a DML trigger. Use DATABASE if the trigger is a DDL database-scoped trigger or ALL SERVER if the trigger is DDL server-scoped trigger."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/QKPpeaZ.png",
+                    "paragraph" : ""
+                },
+                {
+    				"title": "Dropping a Trigger",
+                    "image" : "",
+                    "paragraph" : "The SQL Server DROP TRIGGER statement drops one or more triggers from the database."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/BF6vK4q.png",
+                    "paragraph" : "In this syntax: IF EXISTS conditionally removes the trigger only when it already exists. schema_name is the name of the schema to which the DML trigger belongs. trigger_name is the name of the trigger that you wish to remove. If you want to remove multiple triggers at once, you need to separate triggers by commas."
+                },
+                {
+    				"title": "",
+                    "image" : "https://i.imgur.com/lSwFvyf.png",
+                    "paragraph" : ""
+                }
+    	]
+}');
 
 insert into SECTION(id,name,course_id,information)
 values (8,'Final Quiz',1,'{"id" : "final_quiz"}');
