af4b3d9cdb506b3c63d444f3a4b90c4bfdd5d532
mpapd <valkon.gr@gmail.com>
2020-10-13 22:00:43 +0300

    Redux settings

--- learningplatform-dashboard/src/index.js
+++ learningplatform-dashboard/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   
-
-    <App />,
-
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+    
 );
 
 
